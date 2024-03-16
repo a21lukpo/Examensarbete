7679f16cc7f44be4440f8944b3c53946cb551992
a21lukpo <a21lukpo@student.his.se>
2024-03-16 12:21:07 +0100

    Created a file for asos page
    Created a file that fetches data from Asos dataset using php

---
+++ asosPage.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asos PHP</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+
+    <header>
+        <a href="index.html">
+            <button>Start Page</button>
+        </a>
+
+        <form action="asosPage.php" method="POST">
+            <input type="search" id="searchbar" name="searchbar">
+            <button>knapp</button>
+        </form>
+    </header>
+
+    <?php
+        $servername = "";
+        $username = "";
+        $password = "";
+
+        try{
+            $pdo = new PDO("mysql:host=$servername;dbname=a21lukpo_se_db_2", $username, $password);
+
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e){
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+        if(isset($_POST['searchbar']) && !empty(trim($_POST['searchbar']))){
+
+            $keywords = '%'. $_POST['searchbar'] . '%';
+           
+            $result = "SELECT * FROM products where name like :keywords OR description like :keywords" ;
+            $stmt = $pdo->prepare($result);
+            $stmt->bindParam(':keywords', $keywords);
+            $stmt->execute();
+    
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                echo "<div id='product'>";  
+                    echo "<div id='productName'>" . $row['name'] . "</div>";             
+                    $image = $row['images'];
+                    $image = ltrim($image, "['");
+                    $shownImage = explode(",", $image);
+                    echo "<img src='" . $shownImage[0] . "' alt='image' id='productImage'>";
+                    echo "<div id='productPrice'>" . $row['price'] . " " . $row['currency'] . "</div>"; 
+                    echo "<div id='productDescription'>" . $row['description'] . "</div>";
+                echo "</div>";
+            }
+        }
+    ?>
+
+</body>
+</html>
