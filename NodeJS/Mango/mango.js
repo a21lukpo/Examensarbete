3b3dbc790f2cdcbadb115d966a357ec1c451dac4
a21lukpo <a21lukpo@student.his.se>
2024-04-04 11:18:35 +0200

    Added search functionality
    Added a searchbar and search functionality for the Mango Page with NodeJS. Also created a html file that is connected to the JS file

--- NodeJS/Mango/mango.js
+++ NodeJS/Mango/mango.js
@@ -1,5 +1,9 @@
-const app = require("express")();
-const mysql = require("mysql");
+const express = require('express');
+const app = express();
+const port = 8080;
+const mysql = require('mysql');
+
+app.use(express.urlencoded({ extended: true}));
 
 const connection = mysql.createConnection({
     host: '',
@@ -13,8 +17,26 @@ connection.connect((err) => {
     console.log("Connected");
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
+app.post('/search', (req, res) =>{
+    const searchTerm = req.body.searchbar;
 
-app.get("/", (req, res) => res.send ("Hello world!"));
+    const query = `SELECT * FROM products WHERE name LIKE ?`
 
-app.listen(8080, () => console.log("Listening on port 8080"));
+    connection.query(query, [`%${searchTerm}%`], (error, results, fields) => {
+        if (error){
+            console.error('Error: ', error);
+            res.status(500).send('Error Searching');
+            return;
+        }
+        
+        res.send(results);
+    })
+});
+
+app.listen(port, () => {
+    console.log('Listening on port 8080');
+});
