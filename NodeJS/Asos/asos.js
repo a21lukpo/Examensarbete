b1ca17e2466d5a7e312ceb08d8f524a3d46dc793
a21lukpo <a21lukpo@student.his.se>
2024-04-08 15:13:21 +0200

    Added same code from mango.js
    Added the same code that the mango.js file has and changed the footer so it shows the correct license that is used. Also changed the which database information is fetched from

---
+++ NodeJS/Asos/asos.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const mysql = require('mysql');
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(__dirname));
+
+const connection = mysql.createConnection({
+    host: '',
+    user: '',
+    password: '',
+    database: ''
+});
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log("Connected");
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/search', (req, res) =>{
+    const searchTerm = req.body.searchbar;
+
+    const query = `SELECT * FROM products WHERE name LIKE ? OR description LIKE ?`
+
+    connection.query(query, [`%${searchTerm}%`, `%${searchTerm}%`], (error, results, fields) => {
+        if (error){
+            console.error('Error: ', error);
+            res.status(500).send('Error Searching');
+            return;
+        }
+        
+        let html = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Asos NodeJS</title><link rel="stylesheet" href="stylesheet.css"></head><body>';
+        html += '<header>';
+        html += '<form action="/search" method="POST">';
+        html += '<input type="text" name="searchbar" id="searchbar" value="' + searchTerm + '">';
+        html += '<button type="submit">SÖk!</button>';
+        html += '</form>';
+        html += '</header>';
+        
+        results.forEach(product => {
+            html += `<div id='product'>`;
+                html += `<div id='productName'>${product.name}</div>`;
+                let image = product.images.substring(2, product.images.length - 2);
+                let shownImage = image.split(',');
+                html += `<img src='${shownImage[0]}' alt='Product Image' id='productImage'>`;
+                html += `<div id='productPrice'>${product.price} EUR</div>`;
+                html += `<div id='productDescription'>${product.description}</div>`;
+            html += `</div>`;
+        });
+
+        html += '<footer>';
+        html += ' <p>The data that is used is taken from <a href="https://www.kaggle.com/datasets/trainingdatapro/asos-e-commerce-dataset-30845-products">Kaggle</a> under license <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)</a></p>';
+        html += '</footer>';
+        html += '</body></html>';
+
+        res.send(html);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Listening on port 8080');
+});
